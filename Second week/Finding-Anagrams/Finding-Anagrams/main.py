0fcb9de623de50a0e15f935376a1e4d76254c143
Abdulhameed <abdulhameedararomi@gmail.com>
2022-05-24 22:12:01 +0100

    It worked but not algorithmically efficient, I think

--- Second week/Finding-Anagrams/Finding-Anagrams/main.py
+++ Second week/Finding-Anagrams/Finding-Anagrams/main.py
@@ -4,8 +4,13 @@
 # find_anagrams("racecar") --> True
 
 
-def find_anagrams(word):
+def find_anagrams(word1, word2):
     # [assignment] Add your code here
 
-    return True
+    return cleanstring(word1)==cleanstring(word2)
 
+def cleanstring(word):
+    word = word.lower().split(" ")
+    word = sorted("".join(word))
+
+    return word
