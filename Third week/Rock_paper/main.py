cb118c34f1952ce6543bb876d99e20779ad8ad46
Abdulhameed <abdulhameedararomi@gmail.com>
2022-06-03 16:47:03 +0100

    Made a little change to the code to capitalize user's input

--- Third week/Rock_paper/main.py
+++ Third week/Rock_paper/main.py
@@ -5,7 +5,7 @@ opt = ["R", "P", "S"]
 print("Welcome to Rock-Paper-Scissors game! \nTo save time, know that R represent Rock, P represent Paper and S represent Scissors \nLet's start!")
 
 # Ask for the user input
-user = str(input("Pick an option between R, P and S: "))
+user = str(input("Pick an option between R, P and S: ")).upper()
 computer = random.choice(opt) # The computer choice
 
 dict = {"R":"Rock", "P":"Paper", "S":"Scissors"}
@@ -13,7 +13,7 @@ dict = {"R":"Rock", "P":"Paper", "S":"Scissors"}
 # Check whether the choosen word is among the list of word
 while user not in opt:
     print("Oops! That is not among the list of choice. \n You can only choose 'R', 'P' or 'S'. \n Cheers! You can still choose again!")
-    user = str(input("Pick an option between R, P and S: "))
+    user = str(input("Pick an option between R, P and S: ")).upper()
 
 
 
@@ -22,7 +22,7 @@ while user not in opt:
 while user == computer:    
     print("Player ",dict[user],": CPU ",dict[computer]) 
     print("It is a tie! \nPal, This is getting more interesting! \nLet's try again!")
-    user = str(input("Pick an option between R, P and S: "))
+    user = str(input("Pick an option between R, P and S: ")).upper()
     computer = random.choice(opt)
 
 # Determine the winner!
